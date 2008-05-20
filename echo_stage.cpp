7e27cf7ce82a3831e37e6ac1d34b0953340c146f
penguin673 <penguin673@266760a2-724d-0410-a33e-05c9fb96b1f0>
2008-05-20 03:41:41 +0000

    0.0.6 - nodraw, 4 perspectives (jumping left)

---
+++ echo_stage.cpp
@@ -0,0 +1,93 @@
+// echo_stage.cpp
+
+/*
+    This file is part of L-Echo.
+
+    L-Echo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    L-Echo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with L-Echo.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <typeinfo>
+#include <string>
+
+#include <grid.h>
+#include <echo_stage.h>
+
+stage::stage()
+{
+    grids = new STAGE_MAP();
+}
+
+stage::stage(grid* my_start, std::string my_name, int my_num_goals)
+{
+    grids = new STAGE_MAP();
+    start = my_start;
+    name = my_name;
+    num_goals = my_num_goals;
+}
+
+void stage::add(std::string id, grid* ptr)
+{
+    grids->insert(STAGE_MAP::value_type(id, ptr));
+}
+
+grid* stage::get(std::string id)
+{
+    STAGE_MAP::iterator pos = grids->find(id);
+    if(pos == grids->end())
+        return(NULL);
+    return(pos->second);
+}
+
+void stage::draw(vector3f angle)
+{
+    STAGE_MAP::iterator it = grids->begin();
+    STAGE_MAP::iterator end = grids->end();
+    while(it != end)
+    {
+	    if(it->second->should_draw())
+		    it->second->draw(angle);
+        ++it;
+    }
+}
+
+void stage::set_start(grid* g)
+{
+    start = g;
+}
+
+void stage::set_name(std::string my_name)
+{
+    name = my_name;
+}
+
+void stage::set_num_goals(int my_num_goals)
+{
+    num_goals = my_num_goals;
+}
+
+grid* stage::get_start()
+{
+    return(start);
+}
+
+std::string stage::get_name()
+{
+    return(name);
+}
+
+int stage::get_num_goals()
+{
+    return(num_goals);
+}
---
+++ grid.h
@@ -0,0 +1,88 @@
+// grid.h
+
+/*
+    This file is part of L-Echo.
+
+    L-Echo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    L-Echo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with L-Echo.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <echo_math.h>
+
+#define GRID_SIZE	1
+#define HALF_GRID	0.5f
+
+#ifndef __ECHO_GRID_INFO_T__
+#define __ECHO_GRID_INFO_T__
+typedef struct
+{
+	vector3f pos; //TODO add normal capabilities
+}
+grid_info_t;
+
+void dump_grid_info(grid_info_t ginfo);
+#endif
+
+#define LINE_PT(ln, pt)	((ln) * 4 + (pt))
+#define LINE(ptr, ln)	(ptr)[LINE_PT(ln, 0)], (ptr)[LINE_PT(ln, 1)]
+
+#ifndef __ECHO_CLASS_GRID__
+#define __ECHO_CLASS_GRID__
+class grid
+{
+	protected:
+		line3f* lines;
+		grid_info_t* ginfo;
+		grid** neighbors;
+		int n_neighbors;
+
+		int am_goal;
+		float goal_angle;
+		int draw_me;
+	public:
+		grid();
+		grid(grid_info_t* my_info, grid* my_prev, grid* my_next);
+		grid(grid_info_t* my_info, grid* my_prev, grid* my_next, int num_neighbor);
+		grid(grid_info_t* my_info);
+		virtual void draw(vector3f angle);
+		void init(grid_info_t* my_info, grid* my_prev, grid* my_next);
+		void init(grid_info_t* my_info, grid* my_prev, grid* my_next, int num_neighbor);
+
+		virtual ~grid();
+		virtual grid_info_t* get_info(vector3f angle);
+		virtual grid* get_next(vector3f angle, grid* current);
+		virtual void dump();
+		virtual line3f* get_lines(vector3f angle);
+
+		virtual grid** get_neighbors(vector3f angle);
+		int num_neighbors(vector3f angle);
+		virtual int equals(grid* g, vector3f angle);
+
+		virtual grid* get_real_next();
+		virtual grid* get_real_prev();
+
+		virtual void set_real_next(grid* g);
+		virtual void set_real_prev(grid* g);
+		virtual line3f* generate_lines(grid_info_t my_info);
+
+		virtual void toggle_goal(vector3f angle);
+		virtual int is_goal(vector3f angle);
+		virtual void set_as_goal();
+		
+		virtual int should_draw();
+		virtual void set_draw(int draw);
+
+		void draw_goal(vector3f angle);
+};
+#endif
+
